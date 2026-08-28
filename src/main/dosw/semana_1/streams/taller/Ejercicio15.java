0b97fb67d8afbd27d155e927e3a2b8bb5e55cbce
Oscar10lm <oscar.lasso-m@mail.escuelaing.edu.co>
2026-08-27 21:00:34 -0500

    Taller-ej14-15 S01

---
+++ src/main/dosw/semana_1/streams/taller/Ejercicio15.java
@@ -0,0 +1,35 @@
+package src.main.dosw.semana_1.streams.taller;
+
+import java.util.List;
+
+/**PATRÓN: COMPROBACIÓN NEGATIVA (¿NINGUNO CUMPLE?)
+ * Uso:
+ * "Dada una lista, garantizar que NINGÚN elemento cumpla con una condición
+ * (asegurarse de que la lista esté 'limpia' de algo)".
+ *
+ * ESTRUCTURA CLAVE:
+ * 1. .stream() -> Abre la lista.
+ * 2. .noneMatch( e -> condición ) -> Devuelve true SÓLO si ningún elemento coincide.
+ *    Cierra el proceso.
+ */
+
+public class Ejercicio15 {
+
+    /**
+     * Verifica que el usuario "root" NO exista en la lista de nombres.
+     * Devuelve true porque efectivamente la lista no lo tiene.
+     */
+
+    public static void ejercicio15 () {
+        List<String> nombres = List.of(
+                "juan", "maria", "admin",
+                "pedro", "soporte"
+        );
+        boolean verificar = nombres.stream().noneMatch(n -> n.equals("root"));
+        System.out.println("¿Ninguno es root? " + verificar);
+    }
+
+    public static void main(String[] args) {
+        ejercicio15();
+    }
+}
